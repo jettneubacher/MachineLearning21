60e18bb8589ab907bc34e8d4d19a716081f84424
jettneubacher <jettneubacher9@gmail.com>
2025-07-16 23:08:07 -0400

    update test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import "./css/about.css";
 const About = ({ page, setPage }) => {
   return (
     <div class="about">
-      <h1 class="about-title">Hi Peyton</h1>
+      <h1 class="about-title">Hi Peyton, testing updates rn</h1>
     </div>
   );
 };
